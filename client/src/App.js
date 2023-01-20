922852243f27f04680c5fa68079dea69d9889ab9
offenloa <56451116+offenloa@users.noreply.github.com>
2023-01-20 15:52:29 -0600

    feat: add initial frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+//import './style.css';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import { CardActionArea } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
+  //const [inputGrid, setInputGrid] = useState([[]]);
+  let board_grid = [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="lg">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h3">SCISAT-1</Typography>
+          </Toolbar>
+        </AppBar>
+        <Paper elevation={20}>
+          {board_grid.map((nums, X) => (
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              {nums.map((nums, Y) => (
+                <Box
+                  sx={{
+                    width: '25px',
+                    height: '25px',
+                    border: '2px solid white',
+                    padding: '5px',
+                    backgroundColor:
+                      board_grid[X][Y] === 0 ? 'white' : 'primary.main',
+                    '&:hover': {
+                      width: board_grid[X][Y] === 0 ? '25px' : '29px',
+                      height: board_grid[X][Y] === 0 ? '25px' : '29px',
+                      border:
+                        board_grid[X][Y] === 0
+                          ? '2px solid lightgray'
+                          : '0px',
+                    },
+                  }}
+                ></Box>
+              ))}
+            </Box>
+          ))}
+        </Paper>
+      </Container>
     </div>
   );
 }
