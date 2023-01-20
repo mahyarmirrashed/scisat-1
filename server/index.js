d1851a1a38860f10520dac27cef7751952447899
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-20 16:56:49 -0600

    style: slight modification to server script

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}...`);
 });
